370bf0003965c15bc4e54adab74fdee2b1c0c749
Mayank Raj <mayankrajjha77@gmail.com>
2025-12-12 22:42:57 +0530

    chore: remove SEO content section

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -5,7 +5,6 @@ import { TOOLS } from '@/config/tools';
 import { ToolCard } from '@/components/ToolCard';
 import { Hero } from '@/components/Hero';
 import { ComingSoonModal } from '@/components/ComingSoonModal';
-import { SEOContent } from '@/components/SEOContent';
 
 import { motion } from 'framer-motion';
 
@@ -82,8 +81,6 @@ export default function Home() {
           </motion.div>
         </motion.div>
       </div>
-
-      <SEOContent />
-    </main >
+    </main>
   );
 }
