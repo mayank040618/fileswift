4bbd06b3d82f899a2524c7a187499d523327ef16
Mayank Raj <mayankrajjha77@gmail.com>
2025-12-13 20:45:17 +0530

    chore: add adsense client id

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -74,7 +74,7 @@ export default function RootLayout({
                 {process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID && (
                     <script
                         async
-                        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID}`}
+                        src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=${process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || 'ca-pub-5583723279396814'}`}
                         crossOrigin="anonymous"
                     ></script>
                 )}
