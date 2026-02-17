190e9ac6712c58e3435c42312b20646df796ffc4
Mayank Raj <mayankrajjha77@gmail.com>
2026-02-18 00:41:04 +0530

    fix: use transpilePackages for @imgly

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,18 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    experimental: {
-        serverComponentsExternalPackages: ['@imgly/background-removal', 'onnxruntime-web'],
-    },
-    webpack: (config, { isServer }) => {
+    transpilePackages: ['@imgly/background-removal', 'onnxruntime-web'],
+    webpack: (config) => {
         // pdfjs-dist optionally imports 'canvas' (Node.js native module)
         // which isn't available in the browser. Mark it as external.
         config.resolve.alias.canvas = false;
         config.resolve.alias.encoding = false;
-
-        if (isServer) {
-            config.resolve.alias['@imgly/background-removal'] = false;
-        }
-
         return config;
     },
 };
