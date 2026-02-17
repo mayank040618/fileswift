084b3fd1d4e519d14d018d6f57b01b54ff01de9a
Mayank Raj <mayankrajjha77@gmail.com>
2026-02-18 00:38:32 +0530

    fix: ignore @imgly/background-removal on server build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -3,11 +3,16 @@ const nextConfig = {
     experimental: {
         serverComponentsExternalPackages: ['@imgly/background-removal', 'onnxruntime-web'],
     },
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
         // pdfjs-dist optionally imports 'canvas' (Node.js native module)
         // which isn't available in the browser. Mark it as external.
         config.resolve.alias.canvas = false;
         config.resolve.alias.encoding = false;
+
+        if (isServer) {
+            config.resolve.alias['@imgly/background-removal'] = false;
+        }
+
         return config;
     },
 };
