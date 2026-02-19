00a97446dccd5d1ebe03dd3c0fc72d52dc746cfc
Mayank Raj <mayankrajjha77@gmail.com>
2026-02-20 00:46:00 +0530

    chore: add sharp to serverComponentsExternalPackages in next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        serverComponentsExternalPackages: ['sharp'],
+    },
     webpack: (config, { isServer }) => {
         // pdfjs-dist optionally imports 'canvas' (Node.js native module)
         // which isn't available in the browser. Mark it as external.
